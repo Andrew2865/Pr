a40c697e9671f86c59e6d1f9f52eb8fad75961c6
Andrew <90443266+Andrew2865@users.noreply.github.com>
2021-09-30 09:49:15 +0300

    Add files via upload

---
+++ pr 3.2.cpp
@@ -0,0 +1,30 @@
+﻿// Lab_03_2.cpp
+// < прізвище, ім’я автора >
+// Лабораторна робота No 3.2
+// Розгалуження, задане формулою: функція з параметрами.
+// Варіант 0.1
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double a; // вхідний параметр
+	double b; // вхідний параметр
+	double c; // вхідний параметр
+	double F; // результат обчислення виразу
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+    cout << "x = "; cin >> x;
+	
+	if (a < 0 && x != 0)
+		F =(a*(x*x))+((b*b)*x);
+	else
+		if (a > 0 && x == 0)
+			F = x-(a/(x-c));
+		else
+			F = 1+ (x/c);
+	cout << " F = " << F << endl;
+	cin.get();
+	return 0;
+}
